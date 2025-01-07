d89c9b65b69c80d077c1c643f6e7fb015ebbabc8
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-06 19:19:31 -0500

    module sim io

--- src/main/java/frc/robot/subsystems/drive/module/ModuleIO.java
+++ src/main/java/frc/robot/subsystems/drive/module/ModuleIO.java
@@ -1,4 +1,43 @@
 package frc.robot.subsystems.drive.module;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ModuleIO {
+  @AutoLog
+  class ModuleIOInputs {
+    public boolean driveConnected = false;
+    public double drivePositionDegs = 0.0;
+    public double driveVelocityDegsPerSec = 0.0;
+    public double driveAppliedVolts = 0.0;
+    public double driveCurrentAmps = 0.0;
+
+    public boolean steerConnected = false;
+    public Rotation2d steerPosition = new Rotation2d();
+    public double steerVelocityDegsPerSec = 0.0;
+    public double steerAppliedVolts = 0.0;
+    public double steerCurrentAmps = 0.0;
+
+    public boolean steerEncoderConnected = false;
+    public Rotation2d steerAbsolutePosition = new Rotation2d();
+
+    public double[] odometryTimestamps = new double[] {};
+    public double[] odometryDrivePositionsDegs = new double[] {};
+    public Rotation2d[] odometrySteerPositions = new Rotation2d[] {};
+  }
+
+  // updates the set of logged inputs
+  public default void updateInputs(ModuleIOInputsAutoLogged inputs) {};
+
+  // sets the voltage of the drive motor
+  public default void setDriveOpenLoop(double volts) {};
+
+  // sets the voltage of the steer motor
+  public default void setSteerOpenLoop(double volts) {};
+
+  // sets the drive motor to a certain velocity using onboard PID controllers
+  public default void setDriveVelocity(double degsPerSec) {};
+
+  // sets the steer motor position using onboard PID controllers
+  public default void setSteerPosition(Rotation2d rotation) {};
 }
