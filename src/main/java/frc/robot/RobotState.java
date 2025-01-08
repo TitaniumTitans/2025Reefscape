94f7a90137457dd46e73d28e41a68541844c6c59
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-08 17:05:33 -0500

    work on main drive code, added annotations library

---
+++ src/main/java/frc/robot/RobotState.java
@@ -0,0 +1,60 @@
+package frc.robot;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import lombok.Getter;
+import org.littletonrobotics.junction.AutoLogOutput;
+import frc.robot.subsystems.drive.DriveConstants;
+
+public class RobotState {
+  private static final double poseBufferSizeSec = 2.0;
+  private static final Matrix<N3, N1> odometryStateStdDevs =
+      new Matrix<>(VecBuilder.fill(0.003, 0.003, 0.002));
+
+  private static RobotState instance;
+
+  public static RobotState getInstance() {
+    if (instance == null) instance = new RobotState();
+    return instance;
+  }
+
+  // Pose estimation
+  @Getter
+  @AutoLogOutput(key = "RobotState/OdometryPose")
+  private Pose2d odometryPose = new Pose2d();
+
+  @Getter
+  @AutoLogOutput(key = "RobotState/EstimatedPose")
+  private Pose2d estimatedPose = new Pose2d();
+
+  // used to filter vision measurements into odometry estimation
+  private final TimeInterpolatableBuffer<Pose2d> poseBuffer =
+      TimeInterpolatableBuffer.createBuffer(poseBufferSizeSec);
+  private final Matrix<N3, N1> qStdDevs = new Matrix<>(Nat.N3(), Nat.N1());
+  // Odometry
+  private final SwerveDriveKinematics kinematics;
+  private SwerveModulePosition[] lastWheelPositions =
+      new SwerveModulePosition[] {
+          new SwerveModulePosition(),
+          new SwerveModulePosition(),
+          new SwerveModulePosition(),
+          new SwerveModulePosition()
+      };
+  // Assume gyro starts at zero
+  private Rotation2d gyroOffset = new Rotation2d();
+
+  private RobotState() {
+    for (int i = 0; i < 3; ++i) {
+      qStdDevs.set(i, 0, Math.pow(odometryStateStdDevs.get(i, 0), 2));
+    }
+    kinematics = new SwerveDriveKinematics(DriveConstants.MODULE_TRANSLATIONS);
+  }
+}
--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 
 public class DriveConstants {
+  public static final double ODOMETRY_FREQUENCY = 250;
+  public static final double TRACK_WIDTH_X = Units.inchesToMeters(20.75);
+  public static final double TRACK_WIDTH_Y = Units.inchesToMeters(20.75);
+  public static final Translation2d[] MODULE_TRANSLATIONS = {
+      new Translation2d(TRACK_WIDTH_X / 2, TRACK_WIDTH_Y / 2),
+      new Translation2d(TRACK_WIDTH_X / 2, -TRACK_WIDTH_Y / 2),
+      new Translation2d(-TRACK_WIDTH_X / 2, TRACK_WIDTH_Y / 2),
+      new Translation2d(-TRACK_WIDTH_X / 2, -TRACK_WIDTH_Y / 2)
+  };
+
   public static final double WHEEL_RADIUS_METERS = Units.inchesToMeters(2.0);
 }
