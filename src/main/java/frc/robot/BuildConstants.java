426434aebbda58af32755c190147b1151d148188
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-08 20:07:11 -0500

    command bindings

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "2c90da2581a22875752e840bd3479b2c120bbbc4";
-  public static final String GIT_DATE = "2025-01-08 19:23:57 EST";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "7ff1059dca9a06bca9f444e7dedc8193e5c88bb3";
+  public static final String GIT_DATE = "2025-01-08 19:28:42 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-01-08 19:27:46 EST";
-  public static final long BUILD_UNIX_TIME = 1736382466875L;
+  public static final String BUILD_DATE = "2025-01-08 20:06:41 EST";
+  public static final long BUILD_UNIX_TIME = 1736384801173L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,7 +1,13 @@
 package frc.robot.subsystems.drive;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import org.ironmaple.simulation.drivesims.COTS;
+import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
+import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig;
+
+import static edu.wpi.first.units.Units.*;
 
 public class DriveConstants {
   public static final double ODOMETRY_FREQUENCY = 250;
@@ -19,4 +25,22 @@ public class DriveConstants {
   public static final double MAX_ANGULAR_SPEED = 4.69 / DRIVE_BASE_RADIUS;
 
   public static final double WHEEL_RADIUS_METERS = Units.inchesToMeters(2.0);
+
+  // Pathplanner stuff
+  public static final double WHEEL_COF = 1.2;
+
+  public static final DriveTrainSimulationConfig MAPLE_SIM_CONFIG = DriveTrainSimulationConfig.Default()
+      .withCustomModuleTranslations(MODULE_TRANSLATIONS)
+      .withRobotMass(Pounds.of(115))
+      .withGyro(COTS.ofPigeon2())
+      .withSwerveModule(new SwerveModuleSimulationConfig(
+          DCMotor.getKrakenX60(1),
+          DCMotor.getKrakenX60(1),
+          6.122448979591837,
+          21.428571428571427,
+          Volts.of(0.1),
+          Volts.of(0.1),
+          Meters.of(WHEEL_RADIUS_METERS),
+          KilogramSquareMeters.of(0.02),
+          WHEEL_COF));
 }
