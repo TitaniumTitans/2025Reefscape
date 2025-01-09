42535bfdbdc082d62ff238a49fbc7e13ae962878
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-09 18:45:34 -0500

    build meta data

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,21 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "5e3076bbc3ac0d729ea05cacb3a21f13969708cc";
-  public static final String GIT_DATE = "2025-01-09 17:17:13 EST";
+<<<<<<< HEAD:src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "2bf008890ec82d7b19ffea61436b877bb9099eea";
+  public static final String GIT_DATE = "2025-01-08 19:01:13 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-01-09 17:18:26 EST";
-  public static final long BUILD_UNIX_TIME = 1736461106981L;
+  public static final String BUILD_DATE = "2025-01-08 19:33:36 EST";
+  public static final long BUILD_UNIX_TIME = 1736382816778L;
+=======
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "7bd0ff0ff34ecddf839e751a755178be3c22231d";
+  public static final String GIT_DATE = "2025-01-09 17:18:50 EST";
+  public static final String GIT_BRANCH = "swerve";
+  public static final String BUILD_DATE = "2025-01-09 17:54:27 EST";
+  public static final long BUILD_UNIX_TIME = 1736463267477L;
+>>>>>>> 67cd5002f2b6745de83c594fe582cef9a09fc933:src/main/java/frc/robot/BuildConstants.java
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
