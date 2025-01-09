5e3076bbc3ac0d729ea05cacb3a21f13969708cc
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-09 17:17:13 -0500

    trying sim again, hope it works

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "7ff1059dca9a06bca9f444e7dedc8193e5c88bb3";
-  public static final String GIT_DATE = "2025-01-08 19:28:42 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "a3fd3681f0dd82a504884f52232bfdc25fbb3417";
+  public static final String GIT_DATE = "2025-01-08 20:07:11 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-01-08 20:06:41 EST";
-  public static final long BUILD_UNIX_TIME = 1736384801173L;
+  public static final String BUILD_DATE = "2025-01-08 20:39:43 EST";
+  public static final long BUILD_UNIX_TIME = 1736386783916L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import lombok.Builder;
 import org.ironmaple.simulation.drivesims.COTS;
 import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
 import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig;
@@ -11,8 +13,8 @@ import static edu.wpi.first.units.Units.*;
 
 public class DriveConstants {
   public static final double ODOMETRY_FREQUENCY = 250;
-  public static final double TRACK_WIDTH_X = Units.inchesToMeters(20.75);
-  public static final double TRACK_WIDTH_Y = Units.inchesToMeters(20.75);
+  public static final double TRACK_WIDTH_X = Units.inchesToMeters(26);
+  public static final double TRACK_WIDTH_Y = Units.inchesToMeters(26);
   public static final Translation2d[] MODULE_TRANSLATIONS = {
       new Translation2d(TRACK_WIDTH_X / 2, TRACK_WIDTH_Y / 2),
       new Translation2d(TRACK_WIDTH_X / 2, -TRACK_WIDTH_Y / 2),
@@ -22,9 +24,11 @@ public class DriveConstants {
 
   public static final double DRIVE_BASE_RADIUS = Math.hypot(TRACK_WIDTH_X / 2, TRACK_WIDTH_Y / 2);
   public static final double MAX_LINEAR_SPEED_MPS = Units.feetToMeters(16.0);
-  public static final double MAX_ANGULAR_SPEED = 4.69 / DRIVE_BASE_RADIUS;
+  public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED_MPS / DRIVE_BASE_RADIUS;
 
   public static final double WHEEL_RADIUS_METERS = Units.inchesToMeters(2.0);
+  public static final double DRIVE_GEAR_RATIO = 6.122448979591837;
+  public static final double STEER_GEAR_RATIO = 21.428571428571427;
 
   // Pathplanner stuff
   public static final double WHEEL_COF = 1.2;
@@ -36,11 +40,22 @@ public class DriveConstants {
       .withSwerveModule(new SwerveModuleSimulationConfig(
           DCMotor.getKrakenX60(1),
           DCMotor.getKrakenX60(1),
-          6.122448979591837,
-          21.428571428571427,
+          DRIVE_GEAR_RATIO,
+          STEER_GEAR_RATIO,
           Volts.of(0.1),
           Volts.of(0.1),
           Meters.of(WHEEL_RADIUS_METERS),
           KilogramSquareMeters.of(0.02),
           WHEEL_COF));
+
+  // per module config object
+  @Builder
+  public record ModuleConstants(
+      int driveId,
+      int steerId,
+      int encoderId,
+      Rotation2d encoderOffset,
+      boolean turnInverted,
+      boolean steerInverted
+  ) {}
 }
