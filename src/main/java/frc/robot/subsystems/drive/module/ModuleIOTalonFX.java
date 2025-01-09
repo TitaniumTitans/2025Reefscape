703c5afcb3eb664a907e121a408fdf97dbde7382
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-09 17:18:50 -0500

    it broke again, lets reboot

--- src/main/java/frc/robot/subsystems/drive/module/ModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/drive/module/ModuleIOTalonFX.java
@@ -5,19 +5,20 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import frc.robot.subsystems.drive.DriveConstants;
 
 public class ModuleIOTalonFX implements ModuleIO {
   private final TalonFX driveMotor;
   private final TalonFX steerMotor;
   private final CANcoder encoder;
-  private final SwerveModuleConstants<
-      TalonFXConfiguration,
-      TalonFXConfiguration,
-      CANcoderConfiguration> config;
+  private final DriveConstants.ModuleConstants config;
 
-  public ModuleIOTalonFX( config) {
+  public ModuleIOTalonFX(DriveConstants.ModuleConstants config) {
     this.config = config;
 
+    driveMotor = new TalonFX(config.driveId());
+    steerMotor = new TalonFX(config.steerId());
+    encoder = new CANcoder(config.encoderId());
   }
 
   @Override
