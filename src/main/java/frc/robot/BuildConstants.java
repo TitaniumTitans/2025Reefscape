284680a978e856ca01edca74266aa487cfd29cb5
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-08 19:28:42 -0500

    change desktop sim options

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "c08fc789624344b2fa033fd11a559f24ff839110";
-  public static final String GIT_DATE = "2025-01-08 19:07:26 EST";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "2c90da2581a22875752e840bd3479b2c120bbbc4";
+  public static final String GIT_DATE = "2025-01-08 19:23:57 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-01-08 19:21:19 EST";
-  public static final long BUILD_UNIX_TIME = 1736382079154L;
+  public static final String BUILD_DATE = "2025-01-08 19:27:46 EST";
+  public static final long BUILD_UNIX_TIME = 1736382466875L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
