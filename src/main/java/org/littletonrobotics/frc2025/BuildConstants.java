0bc4759e6c87d581321b1f11886d121da8299732
George <geolin735@gmail.com>
2025-01-08 21:09:44 -0500

    SparkMaxModuleIO and GyroIOPigeon2

--- src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
+++ src/main/java/org/littletonrobotics/frc2025/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "94f7a90137457dd46e73d28e41a68541844c6c59";
-  public static final String GIT_DATE = "2025-01-08 17:05:33 EST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "2bf008890ec82d7b19ffea61436b877bb9099eea";
+  public static final String GIT_DATE = "2025-01-08 19:01:13 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-01-08 18:33:16 EST";
-  public static final long BUILD_UNIX_TIME = 1736379196086L;
+  public static final String BUILD_DATE = "2025-01-08 19:33:36 EST";
+  public static final long BUILD_UNIX_TIME = 1736382816778L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
