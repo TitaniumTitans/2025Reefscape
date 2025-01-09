2c90da2581a22875752e840bd3479b2c120bbbc4
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-08 19:23:57 -0500

    hehe MSVC update needed TwT

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,4 +1,4 @@
-package org.littletonrobotics.frc2025;
+package frc.robot;
 
 /**
  * Automatically generated file containing build version information.
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "94f7a90137457dd46e73d28e41a68541844c6c59";
-  public static final String GIT_DATE = "2025-01-08 17:05:33 EST";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "c08fc789624344b2fa033fd11a559f24ff839110";
+  public static final String GIT_DATE = "2025-01-08 19:07:26 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-01-08 18:33:16 EST";
-  public static final long BUILD_UNIX_TIME = 1736379196086L;
+  public static final String BUILD_DATE = "2025-01-08 19:21:19 EST";
+  public static final long BUILD_UNIX_TIME = 1736382079154L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
