02c003e526de6e9e7074b27102afdfdab55aeee5
georgelin13 <geolin735@gmail.com>
2025-01-12 13:21:50 -0500

    intake subsystem commands

--- src/main/java/frc/robot/subsystems/drive/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/intake/IntakeSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.drive.intake;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -14,6 +15,18 @@ public class IntakeSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         io.updateInputs(inputs);
-        Logger.processInputs("Arm", inputs);
+        Logger.processInputs("Intake", inputs);
+    }
+    public void setIntakePower(double power) {
+        this.io.setMotorVoltageIntake(power);
+    }
+    public void setPivotPower(double power) {
+        this.io.setMotorVoltagePivot(power);
+    }
+    public Command runIntake(double intakePower) {
+        return run(() -> setIntakePower(intakePower));
+    }
+    public Command runPivot(double pivotPower) {
+        return run(() -> setPivotPower(pivotPower));
     }
 }
