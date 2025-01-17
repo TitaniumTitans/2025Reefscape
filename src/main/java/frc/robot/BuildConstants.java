7f93ef2cbb8bd09c0cb28b244687994a4cf0c313
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-17 14:47:37 -0500

    update to 2025 photonlib

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 67;
-  public static final String GIT_SHA = "87a55a659dbd6d7f17d7b43f30cb5ae2b8446bb6";
-  public static final String GIT_DATE = "2025-01-16 17:42:04 EST";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "d1544a2046425428ddf57a25738b7ba3e8cb85af";
+  public static final String GIT_DATE = "2025-01-16 21:00:11 EST";
   public static final String GIT_BRANCH = "vision";
-  public static final String BUILD_DATE = "2025-01-16 20:09:34 EST";
-  public static final long BUILD_UNIX_TIME = 1737076174037L;
+  public static final String BUILD_DATE = "2025-01-17 14:24:31 EST";
+  public static final long BUILD_UNIX_TIME = 1737141871403L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
