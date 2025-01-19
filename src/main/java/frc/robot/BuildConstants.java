aa76c96c2cd033f782d5d8eb955bc8d910e2014e
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-01-19 17:14:47 -0500

    STOP COMMITING BUILDCONSTANTS.JAVA!!!!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 86;
-  public static final String GIT_SHA = "c776f394dda9f741c37aa76dc77bbaf03588c7a5";
-  public static final String GIT_DATE = "2025-01-18 16:26:34 EST";
+  public static final int GIT_REVISION = 89;
+  public static final String GIT_SHA = "bda9fd90e119460836bd346862a8c67174190447";
+  public static final String GIT_DATE = "2025-01-19 14:20:21 EST";
   public static final String GIT_BRANCH = "pathplanner";
-  public static final String BUILD_DATE = "2025-01-18 17:07:24 EST";
-  public static final long BUILD_UNIX_TIME = 1737238044179L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-19 14:57:19 EST";
+  public static final long BUILD_UNIX_TIME = 1737316639731L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
