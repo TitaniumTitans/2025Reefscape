ae21be6c93cb4b16d450a782d94ff5b6c8ea04a2
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-02-15 08:28:07 -0500

    generate all choreo autos

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -4,13 +4,49 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.coralscoral.CoralScoralSubsystem;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.util.ChoreoUtils;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.List;
 
 public class AutoCommands {
+  public enum StartingPositions {
+    StartingPoseLeft,
+    StartingPoseRight,
+    StartingPoseCenter
+  }
+
+  public enum CoralLocations {
+    A,
+    B,
+    AB,
+    C,
+    D,
+    CD,
+    E,
+    F,
+    EF,
+    G,
+    H,
+    GH,
+    I,
+    J,
+    IJ,
+    K,
+    L,
+    KL
+  }
+
+  public enum HumanPlayerSide {
+    HumanPlayerRightFar,
+    HumanPlayerRightClose,
+    HumanPlayerLeftFar,
+    HumanPlayerLeftClose
+  }
+
   public static Command followChoreoPath(String name) {
     try {
       return AutoBuilder.followPath(PathPlannerPath.fromChoreoTrajectory(name));
@@ -24,5 +60,16 @@ public class AutoCommands {
         drive.resetPoseFactory(ChoreoUtils.getPathStartingPose(name).getPose()),
         followChoreoPath(name)
     );
+  }
+
+  public static Command choreoSequence(DriveSubsystem drive,
+                                       CoralScoralSubsystem coral,
+                                       StartingPositions start,
+                                       HumanPlayerSide humanPlayerSide,
+                                       List<CoralLocations> coralPoses) {
+
+    
+
+    return Commands.none();
   }
 }
