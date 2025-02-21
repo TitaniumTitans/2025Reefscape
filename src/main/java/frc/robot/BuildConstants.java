5d9323c7e678e7ce86fea176e645cf7d25f4ff3a
George <geolin735@gmail.com>
2025-02-20 21:10:39 -0500

    fixed vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 151;
-  public static final String GIT_SHA = "6feaf9bca8f7f5d7f79a3b1cfee7b8749049c99d";
-  public static final String GIT_DATE = "2025-02-20 21:02:45 EST";
+  public static final int GIT_REVISION = 153;
+  public static final String GIT_SHA = "f184876bad064f1172e796056d99bb21b393dccf";
+  public static final String GIT_DATE = "2025-02-20 21:07:57 EST";
   public static final String GIT_BRANCH = "LEDs";
-  public static final String BUILD_DATE = "2025-02-20 21:04:43 EST";
-  public static final long BUILD_UNIX_TIME = 1740103483751L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-02-20 21:10:17 EST";
+  public static final long BUILD_UNIX_TIME = 1740103817823L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
