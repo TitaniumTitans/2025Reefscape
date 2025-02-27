5d1f91566a973ede7a3cd6564a069e8ed0d65c6f
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-02-26 20:46:01 -0500

    copy vision back over to new robot

--- oceanus/src/main/java/frc/robot/BuildConstants.java
+++ oceanus/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "oceanus";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 156;
-  public static final String GIT_SHA = "13db3b515152a07e0022b337d0b76d45f2d8c1f4";
-  public static final String GIT_DATE = "2025-02-16 15:15:42 EST";
+  public static final int GIT_REVISION = 169;
+  public static final String GIT_SHA = "6f4004910085f0e52379c808425c470c0856a562";
+  public static final String GIT_DATE = "2025-02-24 14:45:07 EST";
   public static final String GIT_BRANCH = "V2";
-  public static final String BUILD_DATE = "2025-02-16 17:33:32 EST";
-  public static final long BUILD_UNIX_TIME = 1739745212975L;
+  public static final String BUILD_DATE = "2025-02-26 20:43:25 EST";
+  public static final long BUILD_UNIX_TIME = 1740620605563L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
