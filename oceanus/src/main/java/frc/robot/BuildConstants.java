305ae3d840eccbadd001d54477c634eae91a4336
Logan Hollowood <98189655+GearBoxFox@users.noreply.github.com>
2025-04-12 13:08:42 -0400

    change starting pose to 45 degrees

--- oceanus/src/main/java/frc/robot/BuildConstants.java
+++ oceanus/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "oceanus";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 257;
-  public static final String GIT_SHA = "8be7bc84788e68dd0dc32ed8aa828453376bfb9d";
-  public static final String GIT_DATE = "2025-04-11 23:04:18 EDT";
+  public static final int GIT_REVISION = 258;
+  public static final String GIT_SHA = "0543e39b642276dfb101b303a2c1375cfc1db66c";
+  public static final String GIT_DATE = "2025-04-12 13:06:24 EDT";
   public static final String GIT_BRANCH = "SMRUpgrades";
-  public static final String BUILD_DATE = "2025-04-12 13:02:49 EDT";
-  public static final long BUILD_UNIX_TIME = 1744477369118L;
+  public static final String BUILD_DATE = "2025-04-12 13:08:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1744477713422L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
